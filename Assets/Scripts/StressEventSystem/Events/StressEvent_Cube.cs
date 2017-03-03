121c0e2151b512c44c5b58e16641ec70ba29a028
simei.yin <yinsimei>
2017-03-03 18:08:46 +0100

    tutorial example file

---
+++ Assets/Scripts/StressEventSystem/Events/StressEvent_Cube.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StressEvent_Cube : StressEvent_InContainer
+{
+    public GameObject cube;
+    private Color m_originColor;
+    //------------------------------------------------------
+    //  Initialization
+    //------------------------------------------------------
+    protected override void Start()
+    {
+        base.Start();
+        m_originColor = cube.GetComponent<Renderer>().material.color;
+    }
+
+    //------------------------------------------------------
+    //  implement the behavior of the event
+    //------------------------------------------------------
+    public override void StartContainerEvent()
+    {
+        base.StartContainerEvent();
+        cube.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    //------------------------------------------------------
+    //  Reset event values, prepare it for the next call
+    //------------------------------------------------------
+    public override void EndContainerEvent()
+    {
+        base.EndContainerEvent();
+        cube.GetComponent<Renderer>().material.color = m_originColor;
+    }
+}
